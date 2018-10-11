67ae118513a9218c8a371be577770866196a2a2a
daukabase <daukabase@gmail.com>
2018-10-12 00:05:53 +0600

    test: update test cases

--- find_min.py
+++ find_min.py
@@ -4,37 +4,56 @@ def get_min(a, b):
     """
         return min number among a and b
     """
+    return a if a < b else b
 
 
 def get_min_without_arguments():
     """
         raise TypeError exception with message
     """
+    raise TypeError('missed arguments')
 
 
 def get_min_with_one_argument(x):
     """
         return that value
     """
+    return x
 
 
 def get_min_with_many_arguments(*args):
     """
         return smallest number among args
     """
-
+    a = -1
+    if len(args)>0:
+        a = args[0]
+    for i in range(len(args)):
+        if args[i] < a:
+            a = args[i]
+    return a
 
 def get_min_with_one_or_more_arguments(first, *args):
     """
         return smallest number among first + args
     """
-
+    result = float('inf')
+    for arg in args:
+        if result>arg:
+            result = arg
+    if first < result:
+        result = first
+    return result
 
 def get_min_bounded(*args, low, high):
     """
         return smallest number among args bounded by low & high
     """
-
+    result = float('inf')
+    for arg in args:
+        if arg < result and low < arg < high:
+            result = arg
+    return result
 
 def make_min(*, low, high):
     """
@@ -43,8 +62,13 @@ def make_min(*, low, high):
         smallest number is lower than the 'low' which given as required
         argument the inner function has to return it.
     """
-
+    
     def inner(first, *args):
-        pass
+        res = float('inf')
+        for arg in (first,) + args:
+            if arg < res and low < arg < high:
+                res = arg
+        return res
 
     return inner
+
