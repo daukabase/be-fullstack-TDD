8a571b75207ddaf8f2f39b4b22159d9fc61b72f3
daukabase <daukabase@gmail.com>
2018-10-12 00:40:05 +0600

    test: update test cases for find_max

--- find_max.py
+++ find_max.py
@@ -1,29 +1,44 @@
 def get_max(a, b):
-    pass
-
+    return a if a>b else b
 
 def get_max_without_arguments():
-    pass
+    raise TypeError('missed arguments')
 
 
 def get_max_with_one_argument(a):
-    pass
+    return a
 
 
 def get_max_with_many_arguments(*args):
-    pass
+    res = -float('inf')
+    for a in args:
+        if res<a:
+            res = a
+    return res
 
 
 def get_max_with_one_or_more_arguments(first, *args):
-    pass
+    res = -float('inf')
+    for a in (first,) + args:
+        if res<a:
+            res = a
+    return res
 
 
 def get_max_bounded(*args, low, high):
-    pass
+    res = -float('inf')
+    for a in args:
+        if res<a and low<a<high:
+            res = a
+    return res
 
 
 def make_max(*, low, high):
     def inner(first, *args):
-        pass
-
+        res = -float('inf')
+        for a in (first,)+args:
+            if res<a and low<a<high:
+                res = a
+        return res
+    
     return inner
--- tests/find_max_test.py
+++ tests/find_max_test.py
@@ -1,24 +1,39 @@
 from unittest import TestCase
 
+import find_max as f
 
 class FindMaxTest(TestCase):
     def test_get_max(self):
-        pass
+        res = f.get_max(1, 7)
+        self.assertEqual(res, 7)
 
     def test_get_max_without_arguments(self):
-        pass
+        self.assertRaises(TypeError, f.get_max_without_arguments)
 
     def test_get_max_with_one_argument(self):
-        pass
+        res = f.get_max_with_one_argument(2)
+        self.assertEqual(res, 2)
 
     def test_get_max_with_many_arguments(self):
-        pass
+        res = f.get_max_with_many_arguments(2, 3, 5)
+        self.assertEqual(res, 5)
 
     def test_get_max_with_one_or_more_arguments(self):
-        pass
+        res = f.get_max_with_one_or_more_arguments(8, 13, 21)
+        self.assertEqual(res, 21)
 
     def test_get_max_bounded(self):
-        pass
+        kwards = {
+            'low': 21,
+            'high': 34
+        }
+        res = f.get_max_bounded(2, 3, 44, 23, **kwards)
+        self.assertEqual(res, 23)
 
     def test_make_max(self):
-        pass
+        bounded_max = f.make_max(low=0, high=255)
+        self.assertEqual(True, callable(bounded_max))
+        
+        if callable(bounded_max):
+            result = bounded_max(-5, 12, 13)
+            self.assertEqual(13, result)
